073824db2be5916494c81017516aaf9b123bd356
Nolan Hightower <oioioihomelander@yahoo.com>
2024-12-01 20:48:03 -0800

    Inital Commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { ModeWatcher, toggleMode } from 'mode-watcher';
+	import { Button } from '$lib/components/ui/button/index.js';
+
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>Athletic Race Sim</title>
+	<style>
+		html,
+		body {
+			width: 100vw;
+			height: 100vh;
+			display: flex;
+			flex-direction: column;
+		}
+	</style>
+</svelte:head>
+
+<ModeWatcher />
+<main class="relative flex min-h-screen flex-col bg-background">
+	<header
+		class="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
+	>
+		<div class="container flex h-14 w-full max-w-screen-2xl">
+			<div class="ml-auto">
+				<Button variant="outline" class="mt-2" size="icon" onclick={toggleMode}>
+					<span
+						class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+						>🌞</span
+					>
+					<span
+						class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+						>🌙</span
+					>
+				</Button>
+			</div>
+		</div>
+	</header>
+
+	{@render children?.()}
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,237 @@
+<script lang="ts">
+	import * as Table from '$lib/components/ui/table/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { BarChart } from 'layerchart';
+	import { Button, buttonVariants } from '$lib/components/ui/button/index.js';
+	import * as Drawer from '$lib/components/ui/drawer/index.js';
+	import * as Select from '$lib/components/ui/select/index.js';
+
+	interface Result {
+		name: string;
+		time: string;
+		team: {
+			name: string;
+			id: number;
+			icon: string;
+			color: string;
+		};
+	}
+
+	interface TeamResult {
+		name: string;
+		id: number;
+		icon: string;
+		points: number;
+		color: string;
+		athletes: number;
+	}
+
+	let results: Result[] = $state([
+		{ name: 'Alice', time: '1:23', team: { name: 'Team A', id: 1, icon: '🔴', color: 'red' } },
+		{ name: 'Bob', time: '1:45', team: { name: 'Team B', id: 2, icon: '🔵', color: 'blue' } },
+		{ name: 'Charlie', time: '1:30', team: { name: 'Team A', id: 1, icon: '🔴', color: 'red' } },
+		{ name: 'Dave', time: '1:50', team: { name: 'Team C', id: 3, icon: '🟢', color: 'green' } },
+		{ name: 'Dave', time: '1:50', team: { name: 'Team C', id: 3, icon: '🟢', color: 'green' } },
+		{ name: 'Dave', time: '1:50', team: { name: 'Team C', id: 3, icon: '🟢', color: 'green' } },
+		{ name: 'Dave', time: '1:50', team: { name: 'Team C', id: 3, icon: '🟢', color: 'green' } },
+		{ name: 'Dave', time: '1:50', team: { name: 'Team C', id: 3, icon: '🟢', color: 'green' } }
+	]);
+
+	function sortResults(arr: Result[]) {
+		return arr.sort((a, b) => {
+			if (a.time < b.time) return -1;
+			if (a.time > b.time) return 1;
+			return 0;
+		});
+	}
+
+	function updateTime(i: number) {
+		let inputElement = document.getElementById(`t-${i}`);
+		let newTime = inputElement ? (inputElement as HTMLInputElement).value : null;
+
+		if (newTime) {
+			results[i].time = newTime;
+		}
+	}
+
+	function calculateTeamResults(results: Result[]): TeamResult[] {
+		let teams: { [key: number]: TeamResult } = {};
+		results.forEach((result, i) => {
+			if (!teams[result.team.id]) {
+				teams[result.team.id] = {
+					name: result.team.name,
+					id: result.team.id,
+					icon: result.team.icon,
+					color: result.team.color,
+					athletes: 0,
+					points: 0
+				};
+			}
+
+			teams[result.team.id].points += i + 1;
+			teams[result.team.id].athletes += 1;
+		});
+
+		return Object.values(teams).sort((a, b) => a.points - b.points);
+	}
+
+	let teamResults: TeamResult[] = $derived(
+		calculateTeamResults(results).filter((t) => t.athletes >= 5)
+	);
+	let doesntScore = $derived(calculateTeamResults(results).filter((t) => t.athletes < 5));
+
+	let chosenTeam = $state('');
+
+	$effect(() => {
+		results = sortResults(results);
+	});
+
+	let hidden = $state(true);
+</script>
+
+<div class="results pb-5">
+	<Table.Root class="w-[500px]">
+		<Table.Caption>Race Results</Table.Caption>
+		<Table.Header>
+			<Table.Row>
+				<Table.Head>Place</Table.Head>
+				<Table.Head>Name</Table.Head>
+				<Table.Head>Time</Table.Head>
+				<Table.Head>Team</Table.Head>
+				<Table.Head>Actions</Table.Head>
+			</Table.Row>
+		</Table.Header>
+		<Table.Body>
+			{#each results as result, i (i)}
+				<Table.Row>
+					<Table.Cell class="font-medium">{i + 1}</Table.Cell>
+					<Table.Cell>{result.name}</Table.Cell>
+					<Table.Cell>
+						<form onsubmit={() => updateTime(i)}>
+							<Input id="t-{i}" value={result.time} />
+						</form>
+					</Table.Cell>
+					<Table.Cell>
+						<span class="flex items-center">
+							<span>{result.team.icon}</span>
+							<span class="ml-2">{result.team.name}</span>
+						</span>
+					</Table.Cell>
+					<Table.Cell>
+						<Button
+							variant="destructive"
+							onclick={() => {
+								results = results.filter((r, index) => index !== i);
+							}}
+						>
+							Delete
+						</Button>
+					</Table.Cell>
+				</Table.Row>
+			{/each}
+			<Table.Footer>
+				<Drawer.Root>
+					<Drawer.Trigger class={buttonVariants({ variant: 'outline' })}>Add Result</Drawer.Trigger>
+
+					<Drawer.Content>
+						<div class="mx-auto w-full max-w-sm">
+							<Drawer.Header>
+								<Drawer.Title>Move Goal</Drawer.Title>
+								<Drawer.Description>Set your daily activity goal.</Drawer.Description>
+							</Drawer.Header>
+							<div class="p-4 pb-0">
+								<h2>Create a result</h2>
+								<Input id="r-name" placeholder="Name" />
+								<Input id="r-time" class="mb-2 mt-2" placeholder="Time" />
+								<Select.Root type="single" name="chosenTeam" bind:value={chosenTeam}>
+									<Select.Trigger class="w-[180px]"
+										>{chosenTeam === '' ? 'Find a team' : chosenTeam}</Select.Trigger
+									>
+									<Select.Content>
+										<Select.Group>
+											<Select.GroupHeading>Teams</Select.GroupHeading>
+											{#each [...teamResults, ...doesntScore] as team, i (i)}
+												<Select.Item value={team.name}>{team.name}</Select.Item>
+											{/each}
+										</Select.Group>
+									</Select.Content>
+								</Select.Root>
+							</div>
+							<Drawer.Footer>
+								<Button
+									onclick={() => {
+										let data: Result = {
+											name: (document.getElementById('r-name') as HTMLInputElement).value,
+											time: (document.getElementById('r-time') as HTMLInputElement).value,
+											team: teamResults.find((t) => t.name === chosenTeam) ||
+												doesntScore.find((t) => t.name === chosenTeam) || {
+													name: 'No Team',
+													id: 0,
+													icon: '❌',
+													color: 'black'
+												}
+										};
+
+										results = [...results, data];
+									}}>Submit</Button
+								>
+								<Drawer.Close>Cancel</Drawer.Close>
+							</Drawer.Footer>
+						</div>
+					</Drawer.Content>
+				</Drawer.Root>
+			</Table.Footer>
+		</Table.Body>
+	</Table.Root>
+
+	<div class="team-graphs">
+		<h2 class="pb-2">Team Graphs</h2>
+		<div class="h-[300px] rounded border p-4">
+			<BarChart
+				data={teamResults}
+				x="name"
+				y="points"
+				c="id"
+				cRange={teamResults.map((t) => t.color)}
+			/>
+		</div>
+		<h3><strong>Teams that don't score:</strong></h3>
+		<ul>
+			{#each doesntScore as team}
+				<li>{team.name} ({team.athletes} runner(s))</li>
+			{/each}
+		</ul>
+	</div>
+
+	<Button
+		variant="secondary"
+		onclick={() => {
+			navigator.clipboard.writeText(JSON.stringify(results));
+		}}>Copy Results</Button
+	>
+
+	<Button variant="secondary" class="mb-2 mt-2" onclick={() => (hidden = !hidden)}
+		>Input JSON</Button
+	>
+
+	<Button variant="destructive" onclick={() => (results = [])}>Clear Results</Button>
+
+	<div {hidden}>
+		<textarea id="json" class="h-32 w-full rounded border p-2" placeholder="Paste JSON here"
+		></textarea>
+		<Button
+			class="mt-2"
+			onclick={() =>
+				(results = JSON.parse((document.getElementById('json') as HTMLTextAreaElement).value))}
+		>
+			Submit
+		</Button>
+	</div>
+</div>
+
+<style>
+	.results {
+		justify-content: center;
+		margin: 0 auto;
+	}
+</style>
